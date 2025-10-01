39b0776191a8c798f89322e69c2da3b7711a259f
vnxhove <vnxhove@gmail.com>
2025-10-02 04:59:59 +0800

    Update Admin.jsx

--- src/pages/Dasboard/Admin.jsx
+++ src/pages/Dasboard/Admin.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import API from "../../api-server";
 import PageMeta from "../../components/common/PageMeta";
-import TableKkprl from "../../components/tables/AdminTables/TableKKPRL";
+import TableKkprl from "../../components/tables/AdminTables/TableKkprl";
 import Button from "../../components/ui/button/Button";
 
 export default function Admin() {
