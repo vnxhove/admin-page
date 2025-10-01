9013d9b61fb6a2c18ff129bca1501e59f6cc09ae
vnxhove <vnxhove@gmail.com>
2025-10-02 05:47:20 +0800

    testinglagai

--- src/pages/Dasboard/Admin.jsx
+++ src/pages/Dasboard/Admin.jsx
@@ -1,9 +1,5 @@
 import PageMeta from "../../components/common/PageMeta";
 import TableKkprl from "../../components/tables/AdminTables/TableKkprl";
-<<<<<<< HEAD
-=======
-import Button from "../../components/ui/button/Button";
->>>>>>> a1bf0f0365014bd685e255a179552ea439cdae14
 
 export default function Admin() {
   return (
